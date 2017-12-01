2cee6bc53b6736cd4c828a4dac8d02523e2c69f9
jweber9 <jweber@oldcastlebe.com>
2017-11-30 21:23:38 -0700

    updated css

--- MarketingSite/wunc webpage/assets/script.js
+++ MarketingSite/wunc webpage/assets/script.js
@@ -5,3 +5,19 @@ $(window).on("scroll", function() {
        $(".navbar-fixed-top").css("background-color", "rgba(36, 38, 45, .95)");
     }
 });
+
+
+var vid = document.getElementById("myVideo"); 
+
+function playVid() { 
+    vid.play(); 
+} 
+
+function pauseVid() { 
+    vid.pause(); 
+} 
+
+// var vid = document.getElementById("myVideo");
+// vid.onplaying = function() {
+//     alert("The video is now playing");
+// };
