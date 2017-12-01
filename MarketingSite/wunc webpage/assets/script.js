883415b8eff24767edca252bf894d2051ca59da9
jweber9 <jweber@oldcastlebe.com>
2017-12-01 14:46:25 -0700

    added features, app video and made small tweals to layout

--- MarketingSite/wunc webpage/assets/script.js
+++ MarketingSite/wunc webpage/assets/script.js
@@ -1,21 +1,22 @@
 $(window).on("scroll", function() {
-    if($(window).scrollTop() < 1) {
-        $(".navbar-fixed-top").css("background-color", "rgba(36, 38, 45, 0)");
-    } else if ($(window).scrollTop() > 1){
-       $(".navbar-fixed-top").css("background-color", "rgba(36, 38, 45, .95)");
+    if($(window).scrollTop() < 3) {
+        $(".navbar-fixed-top").css("background", "rgba(0, 0, 0, 0)");
+    } else if ($(window).scrollTop() > 3){
+    //    $(".navbar-fixed-top").css("background", "linear-gradient(rgba(0,0,0,.75), rgba(0,0,0,.5), rgba(0,0,0,.25), rgba(0,0,0,0))");
+       $(".navbar-fixed-top").css("background", "rgba(0, 0, 0, .7)");
     }
 });
 
 
-var vid = document.getElementById("myVideo"); 
+// var vid = document.getElementById("myVideo"); 
 
-function playVid() { 
-    vid.play(); 
-} 
+// function playVid() { 
+//     vid.play(); 
+// } 
 
-function pauseVid() { 
-    vid.pause(); 
-} 
+// function pauseVid() { 
+//     vid.pause(); 
+// } 
 
 // var vid = document.getElementById("myVideo");
 // vid.onplaying = function() {
